ac95c91a1659055bc23b77d1018f0ea69593c42a
GiorgioDordo <djole6789@gmail.com>
2024-09-10 21:22:40 +0200

    exercise finished

--- js/script.js
+++ js/script.js
@@ -7,28 +7,29 @@ createApp({
             images:[
                 {
                     image: 'img/1.jpg',
-                    title: 'Marvel\'s Spiderman Miles Morale',
-                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    title: 'Berserk',
+                    text: 'Berserk is a dark fantasy manga series written and illustrated by Kentarō Miura. ',
                 },
                 {
                     image: 'img/2.jpg',
-                    title: 'Ratchet & Clank: Rift Apart',
-                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    title: 'Tokyo Ghoul',
+                    text: 'Tokyo Ghoul is set in an alternate reality where ghouls, creatures that look like normal people but can only survive by eating human flesh, live among the human population in secrecy, hiding their true nature in order to evade pursuit from the authorities.',
                 },
                 {
                     image: 'img/3.jpg',
-                    title: 'Fortnite',
-                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    title: 'Chainsaw Man',
+                    text: "Chainsaw Man follows a lone 16-year-old boy named Denji who, after making a contract with a dog-like devil named Pochita, gains the ability to transform parts of his body into chainsaws",
                 },
                 {
                     image: 'img/4.jpg',
-                    title: 'Stray',
-                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    title: 'Tokyo Ghoul:re',
+                    text: 'Plot. In Tokyo, an unchanging despair is lurking. Mysterious man-eating beings, ghouls, run rampant in Tokyo. Living hidden during everyday life, the existence of ghouls, whose true identities are shrouded in mystery, permanently terrorizes Tokyo\'s residents.',
                 },
+
                 {
                     image: 'img/5.jpg',
-                    title: "Marvel's Avengers",
-                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    title: "Dorohedoro",
+                    text: 'Amnesiac Caiman seeks to undo his lizard head curse by killing the sorcerer responsible, with his friend Nikaido\'s help. In the Hole, that\'s a threat. Amnesiac Caiman seeks to undo his lizard head curse by killing the sorcerer responsible, with his friend Nikaido\'s help.',
                 },
             ],
         }
